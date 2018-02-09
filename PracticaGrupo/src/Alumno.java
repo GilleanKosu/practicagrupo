1c6cbf3a8247b6d5e0b6d70aa81e212e631270b6
DanielGC <hola@hola.com>
2018-02-09 16:47:40 +0100

    Empezando clase Alumno

---
+++ PracticaGrupo/src/Alumno.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+
+public class Alumno {
+	//Atributos
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private String telefono;
+	private String email;
+	private ArrayList<DiaClase> faltas;
+	private ArrayList<Calificacion> notas;
+	
+	/*Métodos:
+Constructor(String dni), que asigna dni y crea los ArrayLists.
+Constructor(String dni, String nombre, String apellidos), que asigna dni,
+nombre y apellidos del alumno y crea los ArrayLists.
+Getters.
+SetDni(), setNombre(), setApellidos(), setTelefono(), setEmail().
+equals(Object object), sobrecarga el método equals de la clase Object. (Os lo
+doy hecho).
+	 * */
+	
+}
