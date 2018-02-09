b9f8f382fee1ce78f46d25345735d06515f64633
DanielGC <hola@hola.com>
2018-02-09 17:21:40 +0100

    Clase Alumno hecha

--- PracticaGrupo/src/Alumno.java
+++ PracticaGrupo/src/Alumno.java
@@ -1,3 +1,4 @@
+//Autor: Daniel Garrido
 
 import java.util.ArrayList;
 
@@ -8,17 +9,82 @@ public class Alumno {
 	private String apellidos;
 	private String telefono;
 	private String email;
-	private ArrayList<DiaClase> faltas;
-	private ArrayList<Calificacion> notas;
+	private ArrayList<DiaClase> faltas;//Contiene las faltas del Alumno para 1 dia de clase (6 horas)
+	private ArrayList<Calificacion> notas;//Contiene las notas del alumno (6 notas)
 	
-	/*Métodos:
-Constructor(String dni), que asigna dni y crea los ArrayLists.
-Constructor(String dni, String nombre, String apellidos), que asigna dni,
-nombre y apellidos del alumno y crea los ArrayLists.
-Getters.
-SetDni(), setNombre(), setApellidos(), setTelefono(), setEmail().
-equals(Object object), sobrecarga el método equals de la clase Object. (Os lo
-doy hecho).
-	 * */
+	//Constructor que asigna dni y crea los ArrayLists.
+	public Alumno(String dni) {
+		this.dni = new String(dni);
+		this.faltas = new ArrayList<DiaClase>();
+		this.notas = new ArrayList<Calificacion>();
+	}
+	//Constructor que asigna dni, nombre y apellidos del alumno y crea los ArrayLists.
+	public Alumno(String dni, String nombre, String apellidos) {
+		this.dni = new String(dni);
+		this.nombre = new String(nombre);
+		this.apellidos = new String(apellidos);
+		this.faltas = new ArrayList<DiaClase>();
+		this.notas = new ArrayList<Calificacion>();
+	}
+	
+	//Getters (devuelven el atributo correspondiente)
+	public String getDni() {
+		return this.dni;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public String getApellidos() {
+		return this.apellidos;
+	}
+	
+	public String getTelefono() {
+		return this.telefono;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public ArrayList<DiaClase> getFaltas() {
+		return this.faltas;
+	}
+	
+	public ArrayList<Calificacion> getNotas() {
+		return this.notas;
+	}
+	
+	//Setters (asignan el valor recibido como argumento al atributo de la clase)
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	// Sobrecarga del método equals de la clase Object
+	public boolean equals(Object object){
+		boolean igual = false;
+
+		if(object instanceof Alumno){
+			Alumno alumno = (Alumno) object;
+			if(alumno.getDni().equalsIgnoreCase(this.getDni()))
+				igual = true;
+		}
+		return igual; 
+	}
 	
 }
