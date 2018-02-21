fd89855187c9071dccb0fbd87c4e81ba86b74c91
Gillean <antoniogarciaillescas@gmail.com>
2018-02-21 16:24:40 +0100

    Corregido error en la clase Fecha que daba error al pasarle el dia

--- PracticaGrupo/src/Fecha.java
+++ PracticaGrupo/src/Fecha.java
@@ -15,17 +15,23 @@ public class Fecha {
 	//Método constructor, que devuelve mensajes de error si el día, mes o año no son correctos
 	public Fecha (int dia, int mes, int agno)  throws Exception{
 		
-		if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 
-				|| mes == 10 || mes == 12 && dia < 1 || dia > 31)
+		if((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 
+				|| mes == 10 || mes == 12) && (dia < 1 || dia > 31)) {
 			throw new Exception("Día incorrecto (Debe estar entre 1 y 31).");
-		if(mes == 4 || mes == 6 || mes == 9 || mes == 11 && dia < 1 || dia > 30)
+		}
+			
+		if(mes == 4 || mes == 6 || mes == 9 || mes == 11 && dia < 1 || dia > 30) {
 			throw new Exception("Día incorrecto (Debe estar entre 1 y 30).");
-		if(mes == 2 && dia < 1 || dia > 28)
+		}
+		if(mes == 2 && dia < 1 || dia > 28) {
 			throw new Exception("Día incorrecto (Debe estar entre 1 y 28).");
-		if(mes < 1 || mes > 12 )
+		}
+		if(mes < 1 || mes > 12 ) {
 			throw new Exception("Mes incorrecto (Debe estar entre 1 y 12).");
-		if(agno < 2016)
+		}
+		if(agno < 2016) {
 			throw new Exception("Año incorrecto (Año anterior a 2016.");
+		}
 	
 		this.dia = dia;
 		this.mes = mes;
